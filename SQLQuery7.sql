bf6dff27561e314c242368a0b1b224e4a23f8d68
tugcezehir <135973617+tugcezehir@users.noreply.github.com>
2025-04-24 21:43:50 +0300

    Update SQLQuery7.sql

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -1,7 +1,7 @@
 USE master;
 GO
 
--- Tüm baðlantýlarý sonlandýr
+
 DECLARE @kill varchar(8000) = '';
 SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), spid) + ';'
 FROM sys.sysprocesses 
@@ -10,6 +10,5 @@ AND spid != @@SPID;
 EXEC(@kill);
 GO
 
--- Þimdi veritabanýný çoklu kullanýcý moduna geçirelim
 ALTER DATABASE AdventureWorks2019 SET MULTI_USER;
 GO
