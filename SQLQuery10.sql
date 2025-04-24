5c9483cbb40f64f23b612abcecc6a8093bcc325b
tugcezehir <135973617+tugcezehir@users.noreply.github.com>
2025-04-24 20:11:32 +0300

    Update SQLQuery10.sql

--- SQLQuery10.sql
+++ SQLQuery10.sql
@@ -1,7 +1,7 @@
 USE AdventureWorks2019;
 GO
 
--- Önce sütunu ekleyelim
+
 IF NOT EXISTS (SELECT 1 FROM DatabaseVersionControl WHERE VersionNumber = '1.1.0')
 BEGIN
     ALTER TABLE HumanResources.Employee
@@ -9,16 +9,16 @@ BEGIN
 END
 GO
 
--- Þimdi yeni eklenen sütun için deðer güncellemesi yapalým
+
 BEGIN TRANSACTION;
 BEGIN TRY
     IF NOT EXISTS (SELECT 1 FROM DatabaseVersionControl WHERE VersionNumber = '1.1.0')
     BEGIN
-        -- Verileri güncelle
+        
         UPDATE HumanResources.Employee
         SET LastPerformanceReviewDate = DATEADD(month, -6, GETDATE());
 
-        -- Versiyon bilgisini güncelleyelim
+        
         INSERT INTO DatabaseVersionControl 
             (VersionNumber, AppliedBy, ScriptName, Description)
         VALUES 
@@ -33,16 +33,15 @@ BEGIN TRY
     COMMIT TRANSACTION;
 END TRY
 BEGIN CATCH
-    -- Hata durumunda geri al
+    
     IF @@TRANCOUNT > 0
         ROLLBACK TRANSACTION;
     
-    -- Hata detaylarýný al
     DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
     DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
     DECLARE @ErrorState INT = ERROR_STATE();
     
-    -- Hatayý logla
+    
     INSERT INTO SchemaChangeLog 
         (EventType, ObjectName, ObjectType, SQLCommand, LoginName)
     VALUES 
@@ -52,7 +51,6 @@ BEGIN CATCH
          @ErrorMessage,
          SYSTEM_USER);
     
-    -- Hatayý fýrlat
     RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
 END CATCH;
 GO
